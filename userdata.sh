41224388d3d7716eca6212f9f6a3aed19e866b30
azurebose <108393935+azurebose@users.noreply.github.com>
2025-01-06 22:04:33 +0530

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+sudo apt-update -y
+sudo apt-get install git -y
+# install Docker  
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu  
+sudo newgrp docker
+sudo chmod 777 /var/run/docker.sock
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker-compose --version
+
+$ docker-compose -f docker-compose/docker-compose-ce.yaml up
+
+$ docker run -e MYSQL_ROOT_PASSWORD= benny@!7678 \
+             -e MYSQL_DATABASE= mariadb_database1 \
+             -e MYSQL_USER= passbolt_user1 \
+             -e MYSQL_PASSWORD= benny@!7678 \ mariadb
+             
+             
+$ docker run --name passbolt \
+             -p 80:80 \
+             -p 443:443 \
+             -e DATASOURCES_DEFAULT_HOST=10.0.0.0/24 \
+             -e DATASOURCES_DEFAULT_PASSWORD= benny@!7678 \
+             -e DATASOURCES_DEFAULT_USERNAME= passbolt_user1 \
+             -e DATASOURCES_DEFAULT_DATABASE= mariadb_database1 \
+             -e APP_FULL_BASE_URL=https://example.com \
+             passbolt/passbolt:develop-debian
+
+$ docker exec passbolt su -m -c "bin/cake passbolt register_user -u bose.kodelli09@email.com -f bose -l surname -r kodelli" -s /bin/sh www-data
