2aad5504a3d62094335f4b8bbfd81157b3140cc7
azurebose <108393935+azurebose@users.noreply.github.com>
2025-01-06 22:09:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket" "example" {
 
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-0fd05997b4dff7aac"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
